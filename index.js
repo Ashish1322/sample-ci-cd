15efce84496f5a49501d5574dcec780fffeadeec
ashish4323 <ashish.k@fwc.co.in>
2023-11-02 12:37:52 +0530

    Sever Setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "App is Running Fine" });
+});
+app.listen(3001, () => console.log("App is Running on port 3001"));
