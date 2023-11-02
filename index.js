6df1c1f857efcf429a713823a6d2e942c7e0d175
ashish4323 <ashish.k@fwc.co.in>
2023-11-02 15:27:28 +0530

    Docker

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ success: true, message: "App is Running Fine" });
 });
-app.listen(3001, () => console.log("App is Running on port 3001"));
+app.listen(3000, () => console.log("App is Running on port 3001"));
